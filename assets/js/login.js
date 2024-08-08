8f948e838b4b81195788aafce742758c5cefc183
Tom Dodd <thomas.dodd@mytheresa.com>
2024-08-08 21:45:03 +0100

    Updated login and homepage

---
+++ assets/js/login.js
@@ -0,0 +1,14 @@
+const validUsername = 'demouser';
+const validPassword = 'fashion123';
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === validUsername && password === validPassword) {
+        window.location.href = 'account.html';
+    } else {
+        document.getElementById('errorMessage').style.display = 'block';
+    }
+});
